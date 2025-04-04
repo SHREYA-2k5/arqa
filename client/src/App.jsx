d72ef940114c270b076d19b9b33b9664e1686367
Not-Buddy <kingeaary@gmail.com>
2025-04-04 19:10:18 +0530

    A routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">Tailwind Test</h1>
-        <p className="text-gray-700">If you see this text styled with a gray color and the heading above in blue with larger text, Tailwind is working correctly!</p>
-        <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Test Button
-        </button>
-      </div>
-    </div>
+    <Router>
+     <div>
+        <div>
+          <Navbar />
+          <div className="flex justify-center"> 
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/start" element={<Start />} />
+            </Routes>
+          </div>
+        </div>
+      </div> 
+    </Router>
   );
 }
 
 export default App;
-
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="sticky top-0 bg-slate-300 shadow-md z-50">
+        <h1 className="text-xl font-bold text-emerald-600 my-4">This will be the navbar</h1>
+    </div>
+  );
+}
+
+export default NavBar;
+
